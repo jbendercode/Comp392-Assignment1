a89c82d8f10911e0900d4f05018b2bbfb4a8fae7
Josh Bender <jbendercode@gmail.com>
2016-01-29 01:22:45 +0000

    11th-Commit

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -265,15 +265,15 @@ function gameLoop(): void {
         }
     }
     
-    if (cubes[98].position.z < -1.1 && punch == true){
+    if (cubes[98].position.z < -0.6 && punch == true){
         cubes[98].position.z += control.superPunchSpeed;
         cubes[104].position.z += control.superPunchSpeed;
     } 
-    else if (cubes[98].position.z > -3 && punch == false){
+    else if (cubes[98].position.z > -2 && punch == false){
         cubes[98].position.z -= control.superPunchSpeed;
         cubes[104].position.z -= control.superPunchSpeed;
     }
-    else if (cubes[98].position.z >= -3 && punch == true){
+    else if (cubes[98].position.z >= -2 && punch == true){
         punch = false;
     }
     else {
